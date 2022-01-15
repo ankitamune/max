5c45fe2000b8710dc8a0339cb74f6c8bba7e5a41
ankitamune <97792393+ankitamune@users.noreply.github.com>
2022-01-15 19:33:12 +0530

    Add files via upload

---
+++ max.java
@@ -0,0 +1,13 @@
+class max{
+    public static void main (String args[]){
+        int num1 =57;
+        int num2 = 35;
+        int num3 = 87;
+        if (num1 >= num2 && num1 >= num3)
+           System.out.println( num1 + " is the maximum number.");
+        else if (num2 >= num1 && num2 >= num3)
+           System.out.println( num2 + " is the maximum number.");
+        else
+           System.out.println( num3 + " is the maximum number.");
+     }
+  }
